2707adf61a9e3b0b4871501f4bed57ae19916432
Midengrows1 <midenfront@gmail.com>
2022-12-09 18:24:44 +0500

    slider new

---
+++ js/index.js
@@ -0,0 +1,11 @@
+swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
